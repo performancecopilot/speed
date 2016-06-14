ea6138fd0e089ab7d15bdad4038732b9b0bf7932
Suyash <dextrous93@gmail.com>
2016-06-14 20:32:09 +0530

    metrics: add benchmark for getHash

--- Makefile
+++ Makefile
@@ -5,3 +5,7 @@ cover: coverage
 coverage:
 	go test -coverprofile=c.out
 	go tool cover -html=c.out
+
+bench: benchmark
+benchmark:
+	go test -bench .
---
+++ metrics_test.go
@@ -0,0 +1,18 @@
+package speed
+
+import "testing"
+
+func BenchmarkGetHash(b *testing.B) {
+	strings := []string{
+		"a",
+		"abcdefghijklmnopqrstuvwxyz",
+		"aaaaaaaaaaaaaaaaaaaaaaaaaa",
+		"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+		"abcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxy",
+	}
+
+	l := len(strings)
+	for i := 0; i < b.N; i++ {
+		_ = getHash(strings[i%l])
+	}
+}
