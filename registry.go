610c1126ea4cd325483c57ded782bdf0bffc6037
Suyash <dextrous93@gmail.com>
2016-06-17 09:47:23 +0530

    registry: modify the Registry interface
    add HasInstanceDomain and HasMetric

--- registry.go
+++ registry.go
@@ -2,9 +2,11 @@ package speed
 
 // Registry defines a valid set of instance domains and metrics
 type Registry interface {
-	AddInstanceDomain(InstanceDomain) error                                                     // adds a InstanceDomain object to the writer
-	AddInstanceDomainByName(name string, instances []string) (InstanceDomain, error)            // adds a InstanceDomain object after constructing it using passed name and instances
-	AddMetric(Metric) error                                                                     // adds a Metric object to the writer
-	AddMetricByName(name string, s MetricSemantics, t MetricType, u MetricUnit) (Metric, error) // adds a Metric object after parsing the name for Instances and InstanceDomains
-	UpdateMetricByName(name string, val interface{}) error                                      // updates a Metric object by looking it up by name and updating its value
+	HasInstanceDomain(name string) bool                                                           // checks if an instance domain of the passed name is already present or not
+	HasMetric(name string) bool                                                                   // checks if an metric of the passed name is already present or not
+	AddInstanceDomain(InstanceDomain) error                                                       // adds a InstanceDomain object to the writer
+	AddInstanceDomainByName(name string, instances []string) (InstanceDomain, error)              // adds a InstanceDomain object after constructing it using passed name and instances
+	AddMetric(Metric) error                                                                       // adds a Metric object to the writer
+	AddMetricByString(name string, s MetricSemantics, t MetricType, u MetricUnit) (Metric, error) // adds a Metric object after parsing the passed string for Instances and InstanceDomains
+	UpdateMetricByName(name string, val interface{}) error                                        // updates a Metric object by looking it up by name and updating its value
 }
