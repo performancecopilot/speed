8fffa2b0fc04c21baa8c432d3b363b3003288178
Suyash <dextrous93@gmail.com>
2016-06-21 07:37:04 +0530

    writer: initial definition of PCPWriter
    This adds the definition of PCPWriter, NewPCPWriter and PCPWriter#length
    
    The PCPWriter has an embedded mutex instead of being as a property,
    because the writer is always supposed to be used in a concurrent context.

--- writer.go
+++ writer.go
@@ -6,6 +6,18 @@ import (
 	"os"
 	"path"
 	"strings"
+	"sync"
+)
+
+// byte lengths of different components in an mmv file
+const (
+	HeaderLength         = 40
+	TocLength            = 16
+	MetricLength         = 104
+	ValueLength          = 32
+	InstanceLength       = 80
+	InstanceDomainLength = 32
+	StringBlockLength    = 256
 )
 
 // Writer defines the interface of a MMV file writer's properties
@@ -29,4 +41,55 @@ func mmvFileLocation(name string) (string, error) {
 	}
 
 	return path.Join(loc, "mmv", name), nil
+}
+
+// PCPWriter implements a writer that can write PCP compatible MMV files
+type PCPWriter struct {
+	sync.Mutex
+	loc string       // absolute location of the mmv file
+	w   io.Writer    // writer to the mmv file
+	r   *PCPRegistry // current registry
+}
+
+// NewPCPWriter initializes a new PCPWriter object
+func NewPCPWriter(name string) (*PCPWriter, error) {
+	fileLocation, err := mmvFileLocation(name)
+	if err != nil {
+		return nil, err
+	}
+
+	w, err := os.Create(fileLocation)
+	if err != nil {
+		return nil, err
+	}
+
+	return &PCPWriter{
+		loc: fileLocation,
+		r:   NewPCPRegistry(),
+		w:   w,
+	}, nil
+}
+
+// Registry returns a writer's registry
+func (w *PCPWriter) Registry() Registry {
+	w.Lock()
+	defer w.Unlock()
+	return w.r
+}
+
+// Length returns the byte length of data in the mmv file written by the current writer
+func (w *PCPWriter) Length() int {
+	w.Lock()
+	defer w.Unlock()
+
+	tocCount := 2
+	if w.Registry().InstanceCount() > 0 {
+		tocCount += 2
+	}
+
+	return HeaderLength +
+		(tocCount * TocLength) +
+		(w.Registry().InstanceCount() * InstanceLength) +
+		(w.Registry().InstanceDomainCount() * InstanceDomainLength) +
+		(w.Registry().MetricCount() * (MetricLength + ValueLength))
 }
