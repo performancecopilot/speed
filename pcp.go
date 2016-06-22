7615235fa73aeb0e247fdeab80e4262daeaa4f88
Suyash <dextrous93@gmail.com>
2016-06-22 19:18:03 +0530

    pcp: add PCPValue

---
+++ pcp.go
@@ -0,0 +1,9 @@
+package speed
+
+// PCPValue defines a set of properties that are supposed to be present in
+// a PCP compatible value, but don't necessarily need to be implemented
+// generally, by *all* values defined in speed
+type PCPValue interface {
+	Offset() int    // returns the memory offset a value is supposed to be stored at
+	SetOffset() int // sets the memory offset
+}
