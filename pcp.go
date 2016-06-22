c5a30fda54c508731d574d3c1ef9db3479a463b1
Suyash <dextrous93@gmail.com>
2016-06-22 20:38:42 +0530

    pcp: fix SetOffset in PCPValue

--- pcp.go
+++ pcp.go
@@ -4,6 +4,6 @@ package speed
 // a PCP compatible value, but don't necessarily need to be implemented
 // generally, by *all* values defined in speed
 type PCPValue interface {
-	Offset() int    // returns the memory offset a value is supposed to be stored at
-	SetOffset() int // sets the memory offset
+	Offset() int   // returns the memory offset a value is supposed to be stored at
+	SetOffset(int) // sets the memory offset
 }
