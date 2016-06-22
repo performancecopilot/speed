45dff61cddcf33e3404b38149b0ddb64d575ea8a
Suyash <dextrous93@gmail.com>
2016-06-22 20:37:21 +0530

    instance: add offset

--- instance.go
+++ instance.go
@@ -2,9 +2,10 @@ package speed
 
 // Instance wraps a PCP compatible Instance
 type Instance struct {
-	name  string
-	id    uint32
-	indom InstanceDomain
+	name   string
+	id     uint32
+	indom  InstanceDomain
+	offset int
 }
 
 // newInstance generates a new Instance type based on the passed parameters
@@ -14,10 +15,14 @@ type Instance struct {
 // but instead added using the AddInstance method of InstanceDomain
 func newInstance(id uint32, name string, indom InstanceDomain) *Instance {
 	return &Instance{
-		name, id, indom,
+		name, id, indom, 0,
 	}
 }
 
 func (i *Instance) String() string {
 	return "Instance: " + i.name
 }
+
+func (i *Instance) Offset() int { return i.offset }
+
+func (i *Instance) SetOffset(offset int) { i.offset = offset }
