6a0eeef7de95f490c6115414b2c3cf3f1f11bdf4
Suyash <dextrous93@gmail.com>
2016-06-23 23:32:39 +0530

    examples: add simple

---
+++ examples/simple.go
@@ -0,0 +1,25 @@
+package main
+
+import "github.com/performancecopilot/speed"
+
+func main() {
+	metric := speed.NewPCPMetric(
+		int32(42),
+		"simple.counter",
+		nil,
+		speed.Int32Type,
+		speed.CounterSemantics,
+		speed.MetricUnit(speed.OneUnit),
+		"",
+		"",
+	)
+
+	writer, err := speed.NewPCPWriter("simple", speed.ProcessFlag)
+	if err != nil {
+		panic(err)
+	}
+
+	writer.Registry().AddMetric(metric)
+
+	writer.Start()
+}
