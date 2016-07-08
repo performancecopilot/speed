22fe7353e5e92347c2a9e064f4cf10df9b0faac2
Suyash <dextrous93@gmail.com>
2016-07-08 19:56:25 +0530

    bytebuffer: add test for WriteInt64

--- bytebuffer/bytebuffer_test.go
+++ bytebuffer/bytebuffer_test.go
@@ -11,10 +11,12 @@ func TestWriteInt32(t *testing.T) {
 		err := b.WriteInt32(val)
 		if err != nil {
 			t.Error(err)
+			return
 		}
 
 		if b.Pos() != 4 {
 			t.Error("Not Writing 4 bytes for int32")
+			return
 		}
 
 		e := []byte{
@@ -32,6 +34,43 @@ func TestWriteInt32(t *testing.T) {
 	}
 }
 
+func TestWriteInt64(t *testing.T) {
+	cases := []int64{0, 10, 100, 200, 1000, 10000, 10000000, 1000000000, 2147483647,
+		4294967295, 10000000000000, 100000000000000000, 9223372036854775807}
+
+	for _, val := range cases {
+		b := NewByteBuffer(8)
+
+		err := b.WriteInt64(val)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		if b.Pos() != 8 {
+			t.Error("Not Writing 8 bytes for int32")
+			return
+		}
+
+		e := []byte{
+			byte(val & 0xFF),
+			byte((val >> 8) & 0xFF),
+			byte((val >> 16) & 0xFF),
+			byte((val >> 24) & 0xFF),
+			byte((val >> 32) & 0xFF),
+			byte((val >> 40) & 0xFF),
+			byte((val >> 48) & 0xFF),
+			byte(val >> 56),
+		}
+
+		for i := 0; i < 8; i++ {
+			if b.buffer[i] != e[i] {
+				t.Errorf("pos: %v, expected: %v, got %v", i, e[i], b.buffer[i])
+			}
+		}
+	}
+}
+
 func TestWriteString(t *testing.T) {
 	cases := []string{"MMV", "Suyash", "This is a little long string"}
 	for _, val := range cases {
@@ -40,10 +79,12 @@ func TestWriteString(t *testing.T) {
 		err := b.WriteString(val)
 		if err != nil {
 			t.Error(err)
+			return
 		}
 
 		if b.Pos() != len(val) {
 			t.Errorf("Expected to write %v bytes, writing %v bytes", len(val), b.Pos())
+			return
 		}
 
 		e := []byte(val)
