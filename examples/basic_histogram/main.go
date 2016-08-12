b3e273b6b4b6ddf80bbcde2de619be9eced26ee4
Suyash <dextrous93@gmail.com>
2016-08-12 22:17:35 +0530

    examples: add a basic histogram example

---
+++ examples/basic_histogram/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/performancecopilot/speed"
+)
+
+func main() {
+	max := int64(100)
+
+	c, err := speed.NewPCPClient("histogram_test")
+	if err != nil {
+		panic(err)
+	}
+
+	m, err := speed.NewPCPHistogram("hist", 0, max, "a sample histogram")
+	if err != nil {
+		panic(err)
+	}
+
+	c.MustRegister(m)
+
+	c.MustStart()
+	defer c.MustStop()
+
+	for i := 0; i < 60; i++ {
+		v := rand.Int63n(max)
+
+		fmt.Println("recording", v)
+		m.MustRecord(v)
+
+		time.Sleep(time.Second)
+	}
+}
